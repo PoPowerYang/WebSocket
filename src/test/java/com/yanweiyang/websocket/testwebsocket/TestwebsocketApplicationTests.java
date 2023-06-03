7aa71a3792424708a84bac57c033b0df183739ad
PoPowerYang <yangsunsunshine@gmail.com>
2023-06-03 15:05:40 -0400

    initial commit

---
+++ src/test/java/com/yanweiyang/websocket/testwebsocket/TestwebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanweiyang.websocket.testwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestwebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
