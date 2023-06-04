64bcf97d21e87dd0603c731981e926a1f542fa66
PoPowerYang <yangsunsunshine@gmail.com>
2023-06-03 22:10:37 -0400

    added pojos

---
+++ src/main/java/com/yanweiyang/websocket/testwebsocket/pojo/GreetingMessage.java
@@ -0,0 +1,19 @@
+package com.yanweiyang.websocket.testwebsocket.pojo;
+
+public class GreetingMessage {
+
+    private String content;
+
+    public GreetingMessage() {
+    }
+
+    public GreetingMessage(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
+
